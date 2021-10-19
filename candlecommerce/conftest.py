525563ba9ea9f4edd3fbab9498199ce336d003b5
DawidCieslewicz <kewid@wp.pl>
2021-10-19 20:02:31 +0200

    init

---
+++ candlecommerce/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from candlecommerce.users.models import User
+from candlecommerce.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
